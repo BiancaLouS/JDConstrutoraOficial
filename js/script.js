f383df0c728a1afe8c237b0f649386152ef69d66
BiancaLouS <biancalourencosilva@gmail.com>
2023-01-18 05:50:56 -0300

    Fazendo alterações para o funcionamento do Carrossel, e manutenção de bugs.

---
+++ js/script.js
@@ -0,0 +1,68 @@
+/* #### CARROSSEL - 1 #### */
+
+$(function(){
+	$('#slide img:eq(0)').addClass('ativo').show();
+
+	var texto = $(".ativo").attr("alt");
+	$('#slide').prepend("<p>"+texto+"</p>");
+	setInterval(slide,5000);
+
+	function slide(){
+
+		if ($('.ativo').next().size()){
+			$('.ativo')
+			.fadeOut()
+			.removeClass("ativo")
+			.next()
+			.fadeIn().addClass("ativo");
+		}else{
+			$('.ativo')
+			.fadeOut()
+			.removeClass("ativo");
+			$('#slide img:eq(0)')
+			.fadeIn()
+			.addClass("ativo");
+		}
+			var texto = $(".ativo").attr("alt");
+			$('#slide p')
+			.hide()
+			.html(texto)
+			.delay(500)
+			.fadeIn();
+	}
+});
+
+/* #### CARROSSEL - 2 #### */
+
+
+$(function(){
+	$('#slide2 img:eq(0)').addClass('ativo2').show();
+
+	var texto = $(".ativo2").attr("alt");
+	$('#slide2').prepend("<p>"+texto+"</p>");
+	setInterval(slide,5000);
+
+	function slide(){
+
+		if ($('.ativo2').next().size()){
+			$('.ativo2')
+			.fadeOut()
+			.removeClass("ativo2")
+			.next()
+			.fadeIn().addClass("ativo2");
+		}else{
+			$('.ativo2')
+			.fadeOut()
+			.removeClass("ativo2");
+			$('#slide2 img:eq(0)')
+			.fadeIn()
+			.addClass("ativo2");
+		}
+			var texto = $(".ativo2").attr("alt");
+			$('#slide2 p')
+			.hide()
+			.html(texto)
+			.delay(500)
+			.fadeIn();
+	}
+});
